faa138ec2cb63eec4fd7c2d24452ed96124b1018
westboy <jin@westboy.net>
2025-08-06 11:40:39 +0800

    refactor(dict): 更新DTO验证注解包路径为jakarta.validation

--- lambda-fusion-dict/src/main/java/com/lambda/fusion/dict/dto/DictStateOperationDTO.java
+++ lambda-fusion-dict/src/main/java/com/lambda/fusion/dict/dto/DictStateOperationDTO.java
@@ -1,8 +1,8 @@
 package com.lambda.fusion.dict.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
