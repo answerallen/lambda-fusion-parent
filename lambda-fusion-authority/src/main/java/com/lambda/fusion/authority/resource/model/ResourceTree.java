4c16dc1830dfab94f8b1675573b8e73cc41cc339
jin <jin@westboy.net>
2026-01-15 14:45:53 +0800

    fix

--- lambda-fusion-authority/src/main/java/com/lambda/fusion/authority/resource/model/ResourceTree.java
+++ lambda-fusion-authority/src/main/java/com/lambda/fusion/authority/resource/model/ResourceTree.java
@@ -1,11 +1,14 @@
 package com.lambda.fusion.authority.resource.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lambda.fusion.core.tree.TreeNode;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -18,7 +21,28 @@ import lombok.EqualsAndHashCode;
 @SuppressFBWarnings("EI_EXPOSE_REP")
 public class ResourceTree extends Resource implements TreeNode<ResourceTree> {
 
+    @Schema(description = "路由组件")
+    private String component;
+
+    @Schema(description = "路由元信息")
+    private Map<String, Object> meta;
+
+    @Schema(description = "路由名称")
+    @JsonProperty("name")
+    @Override
+    public String getResName() {
+        return super.getResName();
+    }
+
+    @Schema(description = "路由路径")
+    @JsonProperty("path")
+    @Override
+    public String getResPath() {
+        return super.getResPath();
+    }
+
     @Schema(description = "子资源集合")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private List<ResourceTree> children;
 
     @Schema(description = "按钮集合")
