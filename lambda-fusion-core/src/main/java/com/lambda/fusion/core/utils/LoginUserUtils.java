1d78879f0bbc14895f5a29f94e8d2bacad98ba63
jin <jin@westboy.net>
2026-01-26 20:04:35 +0800

    fix: Add method to retrieve tenant ID in LoginUserUtils

--- lambda-fusion-core/src/main/java/com/lambda/fusion/core/utils/LoginUserUtils.java
+++ lambda-fusion-core/src/main/java/com/lambda/fusion/core/utils/LoginUserUtils.java
@@ -1,5 +1,6 @@
 package com.lambda.fusion.core.utils;
 
+import cn.hutool.core.util.StrUtil;
 import com.lambda.cloud.core.utils.OperatorUtils;
 import com.lambda.fusion.core.identity.UserPrincipal;
 
@@ -7,5 +8,10 @@ public class LoginUserUtils {
 
     public static UserPrincipal getLoginUser() {
         return OperatorUtils.getLoginUser(UserPrincipal.class);
+    }
+
+    public static String getTenantId() {
+        String tenantId = getLoginUser().getTenantId();
+        return StrUtil.nullToDefault(tenantId,"");
     }
 }
