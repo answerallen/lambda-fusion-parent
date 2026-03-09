eb67d199d5874bc8e000c9852f7eb591bde59d8f
jin <jin@westboy.net>
2026-03-09 16:16:42 +0800

    Remove unused properties and `DevRole` class from `AuthorityProperties`

--- lambda-fusion-authority/src/main/java/com/lambda/fusion/authority/AuthorityProperties.java
+++ lambda-fusion-authority/src/main/java/com/lambda/fusion/authority/AuthorityProperties.java
@@ -1,9 +1,6 @@
 package com.lambda.fusion.authority;
 
-import cn.hutool.core.collection.CollUtil;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,11 +12,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @SuppressFBWarnings("EI_EXPOSE_REP")
 public class AuthorityProperties {
     /**
-     * 是否开启数据角色
-     */
-    private boolean enabledDataRole = false;
-
-    /**
      * 组织名称作为 id 使用并入库
      */
     private boolean useOrgNameAsId = false;
@@ -28,10 +20,6 @@ public class AuthorityProperties {
      * 密码策略
      */
     private PasswordStrategy passwordStrategy = new PasswordStrategy();
-    /**
-     * 开发者角色配置
-     */
-    private DevRole dev = new DevRole();
 
     /**
      * 密码策略
@@ -73,22 +61,6 @@ public class AuthorityProperties {
              * 加密值
              */
             CIPHERTEXT
-        }
-    }
-
-    @Getter
-    @Setter
-    public static class DevRole {
-        private final String[] defaultWhiteArray = new String[] {
-            "/public/**", "**/dict/**", "/dict/**", "/monitor/**", "/error/**", "/error", "/v3/**",
-        };
-        private List<String> whiteList;
-
-        public List<String> getWhiteList() {
-            List<String> list = new ArrayList<>();
-            CollUtil.addAll(list, whiteList);
-            CollUtil.addAll(list, defaultWhiteArray);
-            return list;
         }
     }
 }
