7a5d5dc88e37decdba7403b49828f25d02526e67
jin <jin@westboy.net>
2026-07-29 13:58:18 +0800

    fix

--- docs/sql/la_role_groups.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `la_role_groups` (`GROUP_ID`, `GROUP_NAME`, `TENANT_ID`) VALUES ('2016824170421256194', '运营商组', NULL);
-INSERT INTO `la_role_groups` (`GROUP_ID`, `GROUP_NAME`, `TENANT_ID`) VALUES ('default', '默认分组', NULL);
